67f3a14377fe415679d462f5c61cfdd8144cd01f
t.yoshida <t.yoshida@ebacorp.jp>
2022-01-29 12:31:23 +0900

    画像の投稿機能実装完了

--- src/components/BoadList.vue
+++ src/components/BoadList.vue
@@ -5,6 +5,7 @@
   <ul v-else class="list-group text-left">
     <li v-for="(item, key) in data.board_data" :key="key" class="list-group-item">
       <div class="h5">{{item.msg}}</div>
+      <div class="image"><img :src="item.photoURL" alt="" width="100"></div>
       <div class="small text-right"><router-link :to="{name:'Profile',params:{prmUid:item.user}}">{{item.displayName}}</router-link> ({{item.posted}})</div>
       <span @click="toggle_like" class="btn-like" :class="{on:item.likeOn}" :data-id="item.key"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"/></svg></span>
       <span v-if="item.like" class="like-cnt">{{Object.keys(item.like).length}}</span>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,6 +9,11 @@
         <label class="h5">Message</label>
         <textarea v-model.trim="data.msg" rows="5" class="form-control" placeholder="What’s happening"></textarea>
         <p class="msg-counter"><span>{{data.msg.length}}</span>/{{data.msg_maxlength}}</p>
+        <div class="upload">
+          <p class="photo"><img :src="data.uploadImageUrl" alt="" width="100"></p>
+          <input type="file" accept="image/*" name="inputProfileFile" show-size @change="onImagePicked">
+          <input type="button" value="クリア" @click="clearFileInput">
+        </div>
         <button @click="add" :disabled="addBtn_disabled" class="btn btn-primary">投稿</button>
       </div>
       <h3 class="my-3">フォロー中のユーザーの投稿</h3>
@@ -31,6 +36,7 @@ import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 import { logout, auth } from '../plugins/auth'
 import * as commonJS from '../plugins/common'
+import { attachImage } from '../plugins/auth'
 
 import BoadList from './BoadList.vue'
 
@@ -47,6 +53,10 @@ export default {
       message: 'ミニ伝言板。最新の投稿を表示します。',
       msg: '',
       msg_maxlength: commonJS.MESSAGE_MAX_LENGTH,
+
+      uploadImageUrl: "",//photoアップロード関連
+      input_image:null,//photoアップロード関連
+
       router: useRouter(),
       store: useStore()
     })
@@ -76,6 +86,31 @@ export default {
         console.log(error)
       })
     }
+    
+    //写真のアップロード
+    const onImagePicked = (e)=>{
+      const file = e.target.files[0]
+      if (file !== undefined && file !== null) {
+        if (file.name.lastIndexOf('.') < 0) {
+          return
+        }
+        data.input_image = file
+        const fr = new FileReader()
+        fr.readAsDataURL(file)
+        fr.addEventListener('load', () => {
+          data.uploadImageUrl = fr.result
+        })
+      } else {
+        data.uploadImageUrl = ''
+        data.input_image = null
+      }
+    }
+    //input:fileのクリアボタン
+    const clearFileInput = ()=>{
+      document.getElementsByName('inputProfileFile')[0].value = ''
+      data.uploadImageUrl = ''
+      data.input_image = null
+    }
 
     // メッセージ追加
     const add = ()=> {
@@ -88,14 +123,31 @@ export default {
       let dstr = d.getFullYear() + '-' + (d.getMonth() + 1) + '-'
         + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes()
         + ':' + d.getSeconds()
+      
       let obj = {
         msg: data.msg,
         user: data.store.state.uid,
         posted: dstr,
       }
-      db_board.push(obj)
-      data.msg = ''
-      data.message = '投稿しました。'
+      
+      if(data.input_image){
+        attachImage({
+          inputFile: data.input_image,
+          directory: data.store.state.uid + '/msg/',
+          resultFnc: (downloadURL)=>{
+            // console.log('File available at', downloadURL)
+            obj.photoURL = downloadURL
+            db_board.push(obj)
+            data.msg = ''
+            clearFileInput()
+            data.message = '投稿しました。'
+          }
+        })
+      }else{
+        db_board.push(obj)
+        data.msg = ''
+        data.message = '投稿しました。'
+      }
     }
 
     // メッセージが入力されたとき
@@ -114,7 +166,7 @@ export default {
         data.router.push('/signin')
       }
     })
-    return { data, init, doLogout, add, addBtn_disabled, msg_cntOver }
+    return { data, init, doLogout, onImagePicked, clearFileInput, add, addBtn_disabled, msg_cntOver }
   },
 }
 </script>
